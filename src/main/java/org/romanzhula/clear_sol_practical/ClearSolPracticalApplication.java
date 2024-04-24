0a4f48f25ac5be128be9f5065de16b8020842fe2
RoMANzhula <roman1985al@ukr.net>
2024-04-24 22:03:43 +0300

    First commit

---
+++ src/main/java/org/romanzhula/clear_sol_practical/ClearSolPracticalApplication.java
@@ -0,0 +1,13 @@
+package org.romanzhula.clear_sol_practical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSolPracticalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearSolPracticalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/romanzhula/clear_sol_practical/ClearSolPracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.romanzhula.clear_sol_practical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolPracticalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
