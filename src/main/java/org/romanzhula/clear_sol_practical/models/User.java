8592c9ecb349219490842d8ccb737650729a8d5c
RoMANzhula <roman1985al@ukr.net>
2024-04-25 00:48:53 +0300

    Add UserDTO and several validate

--- src/main/java/org/romanzhula/clear_sol_practical/models/User.java
+++ src/main/java/org/romanzhula/clear_sol_practical/models/User.java
@@ -21,37 +21,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
-    @NotBlank(message = "Email cannot be empty!")
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
-            message = "Invalid email format. Only letters, numbers, dots, and one @ symbol.")
-    @Email
+    @Column(name = "email")
     private String email;
 
-    @Column(name = "first_name", nullable = false)
-    @NotBlank(message = "User first name cannot be empty!")
-    @Pattern(regexp = "^[A-Z][a-zA-Z]*$", message = "Invalid characters in first name. Only letters are allowed.")
-    @Length(max = 255, message = "Message too long (limit - 255 B)")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
-    @NotBlank(message = "User last name cannot be empty!")
-    @Pattern(regexp = "^[A-Z][a-zA-Z]*$", message = "Invalid characters in last name. Only letters are allowed.")
-    @Length(max = 255, message = "Message too long (limit - 255 B)")
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "birth_date", nullable = false)
-    @Temporal(value = TemporalType.DATE)
-    @Past(message = "Only past data!")
-    @Min(value = 6570, message = "User must be over 18 years old!")
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
-    @Length(max = 255, message = "Message too long (limit - 255 B)")
+    @Column(name = "address")
     private String address;
 
-    @Column(unique = true)
-    @NotBlank(message = "User phone number cannot be empty!")
-    @Pattern(regexp = "^(00\\d+|0\\d+|\\+\\d+)$", message = "Invalid phone number format")
-    @Length(min = 9, max = 15, message = "Number is short/long (limit - from 9 to 15 characters)")
+    @Column(name = "phone_number")
     private String phoneNumber;
+
 }
