66f993dfcce3238b5d23261dc13ed47abd37bd8f
RoMANzhula <roman1985al@ukr.net>
2024-04-25 18:53:41 +0300

    Add JSON response for registration method Fix ResponseEntity to JSON answers

--- src/main/java/org/romanzhula/clear_sol_practical/controllers/UserController.java
+++ src/main/java/org/romanzhula/clear_sol_practical/controllers/UserController.java
@@ -3,6 +3,7 @@ package org.romanzhula.clear_sol_practical.controllers;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.romanzhula.clear_sol_practical.dto.UserDTO;
+import org.romanzhula.clear_sol_practical.json_responsies.RegistrationResponse;
 import org.romanzhula.clear_sol_practical.models.User;
 import org.romanzhula.clear_sol_practical.repositories.UserRepository;
 import org.romanzhula.clear_sol_practical.services.UserService;
@@ -27,7 +28,7 @@ public class UserController {
     private final UserRepository userRepository;
 
     @PostMapping("/registration")
-    public ResponseEntity<String> createUser(
+    public ResponseEntity<RegistrationResponse> createUser(
             @ModelAttribute("userDTO")
             @Valid UserDTO userDTO,
             BindingResult bindingResult
@@ -35,16 +36,22 @@ public class UserController {
         if (!bindingResult.hasErrors()) {
             logger.error("Validation error! Check your input data.");
 
-            return ResponseEntity.badRequest().body("Validation error! Check your input data.");
+            return ResponseEntity
+                    .badRequest()
+                    .body(new RegistrationResponse("Validation error! Check your input data."))
+            ;
         }
 
         userService.registerNewUser(userDTO);
 
-        return ResponseEntity.ok("User created successfully");
+        return ResponseEntity
+                .ok()
+                .body(new RegistrationResponse("User created successfully"))
+        ;
     }
 
     @PatchMapping("/update-field/{userId}")
-    public ResponseEntity<String> updateUserField(
+    public ResponseEntity<?> updateUserField(
             @PathVariable Long userId,
             @RequestBody @Valid UserDTO userDTO,
             BindingResult bindingResult
@@ -52,17 +59,23 @@ public class UserController {
         if (!bindingResult.hasErrors()) {
             logger.error("Validation error! Check your input update data.");
 
-            return ResponseEntity.badRequest().body("Validation error! Check your input update data.");
+            return ResponseEntity
+                    .badRequest()
+                    .body("Validation error! Check your input update data.")
+            ;
         }
 
         userService.updateUserByField(userId, userDTO);
 
-        return ResponseEntity.ok("User's field(s) updated successfully");
+        return ResponseEntity
+                .ok()
+                .body("User's field(s) updated successfully")
+        ;
 
     }
 
     @PutMapping("/whole-update/{userId}")
-    public ResponseEntity<String> updateWholeUser(
+    public ResponseEntity<?> updateWholeUser(
         @PathVariable Long userId,
         @RequestBody @Valid UserDTO userDTO,
         BindingResult bindingResult
@@ -75,21 +88,30 @@ public class UserController {
 
         userService.updateWholeUser(userDTO, userId);
 
-        return ResponseEntity.ok("User with id: " + userId + " was updated whole successfully!");
+        return ResponseEntity
+                .ok()
+                .body("User with id: " + userId + " was updated whole successfully!")
+        ;
     }
 
     @DeleteMapping("/remove-user/{userId}")
-    public ResponseEntity<String> removeUserById(
+    public ResponseEntity<?> removeUserById(
             @PathVariable Long userId
     ) {
         if (userRepository.existsById(userId)) {
             userRepository.deleteById(userId);
 
-            return ResponseEntity.ok("User deleted successfully!");
+            return ResponseEntity
+                    .ok()
+                    .body("User deleted successfully!")
+            ;
         }
 
         logger.error("ERROR: User with id {} NOT FOUND!", userId);
-        return ResponseEntity.notFound().build();
+        return ResponseEntity
+                .notFound()
+                .build()
+        ;
     }
 
     @GetMapping("/birthday-range")
@@ -98,11 +120,17 @@ public class UserController {
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate to
     ) {
         if (from.isAfter(to)) {
-            return ResponseEntity.badRequest().body("Date From should be less than date To.");
+            return ResponseEntity
+                    .badRequest()
+                    .body("Date From should be less than date To.")
+            ;
         }
 
         List<User> users = userRepository.findByBirthDateBetween(from, to);
-        return ResponseEntity.ok(users);
+        return ResponseEntity
+                .ok()
+                .body(users)
+        ;
     }
 
     @ModelAttribute("userDTO")
---
+++ src/main/java/org/romanzhula/clear_sol_practical/json_responsies/RegistrationResponse.java
@@ -0,0 +1,14 @@
+package org.romanzhula.clear_sol_practical.json_responsies;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationResponse {
+    private String message;
+}
