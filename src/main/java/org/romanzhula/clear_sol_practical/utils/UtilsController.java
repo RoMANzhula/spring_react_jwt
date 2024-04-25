18d764ff36c30c2b8c71016e11c42394a389bd9b
RoMANzhula <roman1985al@ukr.net>
2024-04-25 21:16:40 +0300

    Add wrapper for BindingResult

---
+++ src/main/java/org/romanzhula/clear_sol_practical/utils/UtilsController.java
@@ -0,0 +1,25 @@
+package org.romanzhula.clear_sol_practical.utils;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class UtilsController {
+    public static Map<String, String> getBindingErrors(BindingResult bindingResult) {
+        Collector<FieldError, ?, Map<String, String>> collector = Collectors
+                .toMap(
+                    fieldError -> fieldError.getField() + "ERROR",
+                    FieldError::getDefaultMessage
+                )
+        ;
+
+        return bindingResult
+                .getFieldErrors()
+                .stream()
+                .collect(collector)
+        ;
+    }
+}
