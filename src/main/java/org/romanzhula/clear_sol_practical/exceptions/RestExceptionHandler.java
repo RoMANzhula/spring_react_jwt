6cc146e98ad180f8a5e171563e6ac7ae0af673fb
RoMANzhula <roman1985al@ukr.net>
2024-04-25 21:15:39 +0300

    Add rest exception handler for BadRequest status

---
+++ src/main/java/org/romanzhula/clear_sol_practical/exceptions/RestExceptionHandler.java
@@ -0,0 +1,22 @@
+package org.romanzhula.clear_sol_practical.exceptions;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(exception.getBindingResult().getFieldError().getDefaultMessage());
+    }
+}
