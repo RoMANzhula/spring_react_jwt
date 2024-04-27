0117373beb38906719e83cec16318ff651e8a247
RoMANzhula <roman1985al@ukr.net>
2024-04-27 17:49:55 +0300

    Add tests for whole update user method

---
+++ src/test/resources/sql/create-user-before.sql
@@ -0,0 +1,4 @@
+DELETE FROM users;
+
+INSERT INTO users (user_id, email, first_name, last_name, birth_date, address, phone_number)
+VALUES (1, 'testemail11@test.test', 'Namenn', 'Surnamenn', '1981-10-10', 'Main 11 Street', '1234767891');
