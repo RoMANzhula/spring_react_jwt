1f1e7dfe52f9e98d7b4ad5dfa9446f11389436f1
RoMANzhula <roman1985al@ukr.net>
2024-04-30 21:37:40 +0300

    Add logout mapping

--- src/main/java/org/romanzhula/clear_sol_practical/controllers/AuthenticationController.java
+++ src/main/java/org/romanzhula/clear_sol_practical/controllers/AuthenticationController.java
@@ -1,5 +1,7 @@
 package org.romanzhula.clear_sol_practical.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.romanzhula.clear_sol_practical.controllers.json_requests.LoginStaffRequest;
@@ -10,6 +12,9 @@ import org.romanzhula.clear_sol_practical.utils.UtilsController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -70,4 +75,25 @@ public class AuthenticationController {
 
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> handleLogout(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+            if (auth != null) {
+                new SecurityContextLogoutHandler().logout(request, response, auth);
+                return ResponseEntity.ok("Logout successful");
+            } else {
+                return ResponseEntity.status(401).body("Not authenticated");
+            }
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Internal Server Error");
+        }
+    }
+
 }
