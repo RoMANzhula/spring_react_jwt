c48a1a6075187e7f9c87ef2341429c1ca408951c
RoMANzhula <roman1985al@ukr.net>
2024-05-01 21:42:02 +0300

    Add component adminContent on the front side

---
+++ src/main/frontend/src/components/adminContent/AdminContent.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import UserService from "../../services/user.service";
+import eventBus from "../../common/EventBus";
+
+
+const AdminContent = () => {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    UserService.getAdminContent().then(
+      (response) => {
+        setContent(response.data);
+      },
+      (error) => {
+        const contentValue =
+          (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setContent(contentValue);
+
+        if (error.response && error.response.status === 401) {
+          eventBus.dispatch("logout");
+        }
+      }
+    );
+  }, []);
+
+  return (
+    <div className="container">
+      <header className="container">
+        <h2>{content}</h2>
+      </header>
+    </div>
+  );
+};
+
+export default AdminContent;
+
