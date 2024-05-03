3406a995d1e9fd82a0c04035e0fe6abc45d52b1f
RoMANzhula <roman1985al@ukr.net>
2024-05-03 19:28:16 +0300

    Add home component

---
+++ src/main/frontend/src/components/homePage/Home.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react"
+import UserService from "../../services/user.service";
+
+
+const Home = () => {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    UserService.getPublicContent().then(
+      (response) => {
+        setContent(response.data);
+      },
+      (error) => {
+        const contentValue =
+          (error.response &&
+          error.response.data) ||
+          error.message ||
+          error.toString();
+
+        setContent(contentValue);
+      }
+    );
+  }, []);
+
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h2>{content}</h2>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
