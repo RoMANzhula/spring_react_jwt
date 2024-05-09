19baa52117da06dca451a18c1d7c9d1b9e719d57
RoMANzhula <roman1985al@ukr.net>
2024-05-09 23:25:27 +0300

    Add CORS cust configurations for PUT and PATCH mapping

--- src/main/java/org/romanzhula/clear_sol_practical/configurations/WebMvcConfiguration.java
+++ src/main/java/org/romanzhula/clear_sol_practical/configurations/WebMvcConfiguration.java
@@ -13,13 +13,17 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Value("${allowed_cross_origin}")
     private String allowedOrigin;
 
+    private String[] allowedMethods = {"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"};
 
     @Bean
     public WebMvcConfigurer configure() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry reg) {
-                reg.addMapping("/**").allowedOrigins(allowedOrigin);
+                reg
+                        .addMapping("/**").allowedOrigins(allowedOrigin)
+                        .allowedMethods(allowedMethods)
+                ;
             }
         };
     }
